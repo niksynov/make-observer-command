7d5ea9a9368bac68bccd3e7b3e64c8875889f742
vagrant <vagrant@machine1.puphpet>
2017-11-19 12:41:48 +0000

    First one

---
+++ src/MakeObserver.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace NickSynev\MakeObserverCommand;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class MakeObserver extends Command
+{
+    const CONST_VALID_METHODS = ['creating', 'created', 'updating', 'updated', 'deleting', 'deleted'];
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:observer {name} {model} {--methods=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create observer';
+
+    protected $methods = [];
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $makeMethods = $this->makeMethods();
+
+        $observerName = $this->argument('name');
+
+        if ($makeMethods === false) {
+
+            $this->error('Method is not allowed!');
+
+            return false;
+        }
+
+        $observersDir = app_path('Observers');
+
+        if (!File::exists($observersDir)) {
+
+            File::makeDirectory($observersDir);
+
+        }
+
+        if (File::exists($observersDir . "/{$observerName}.php")) {
+
+            if (!$this->confirm('Observer already exists, do you wish to override it?')) {
+
+                return false;
+
+            } else {
+
+                File::delete($observersDir . '/' . $observerName . '.php');
+
+                $this->addObserverToDirectory($this->methods);
+
+                return false;
+            }
+        }
+
+        $observerName = explode("/", $observerName);
+
+        for ($i = 0; $i < count($observerName); $i++) {
+
+            if ($i != count($observerName) - 1) {
+
+                if(!file_exists($observersDir . '/' . $observerName[$i])){
+
+                    File::makeDirectory($observersDir . '/' . $observerName[$i]);
+
+                }
+
+                $observersDir = $observersDir . '/' . $observerName[$i];
+            }
+        }
+
+        $this->addObserverToDirectory($this->methods);
+    }
+
+    private function makeMethods()
+    {
+        $methods = $this->options()['methods'] != null ? explode(',', $this->options()['methods']) : null;
+
+        if ($methods) {
+
+            foreach ($methods as $method) {
+
+                if (!in_array($method, self::CONST_VALID_METHODS)) {
+
+                    return false;
+
+                } else {
+
+                    array_push($this->methods, $method);
+                }
+            }
+
+            return true;
+        }
+    }
+
+    private function addObserverToDirectory($methods = [])
+    {
+        $observerDirectory = $this->argument('name');
+
+        $observerName = explode("/", $this->argument('name'));
+
+        $observerNameForNamespace = end($observerName);
+
+        $observerName = studly_case($observerNameForNamespace);
+
+        $observerNamespace = str_replace("/", '\\', $observerDirectory);
+
+        $observerNamespace = str_replace('\\' . $observerNameForNamespace, '', $observerNamespace);
+
+        $observerModel = $this->argument('model');
+
+        $observerModelInjection = explode("\\", $observerModel);
+
+        $observerModelInjection = end($observerModelInjection) . " $" . camel_case(end($observerModelInjection));
+
+        $observerStub = file_get_contents(__DIR__. '/Stubs/Observer/' . 'observer.stub', true);
+
+        $observerContent = '';
+g
+        $methods = $methods ?: self::CONST_VALID_METHODS;
+
+        foreach ($methods as $method) {
+
+            if ($method === end($methods)) {
+
+                $observerContent .= file_get_contents(__DIR__. '/Stubs/Observer/Methods/' . $method . '.stub', true) . "\n";
+
+                break;
+            }
+
+            $observerContent .= file_get_contents(__DIR__. '/Stubs/Observer/Methods/' . $method . '.stub', true) . "\n\n";
+        }
+
+        $dummyStrings = ['DummyNamespace', 'DummyClass', 'DummyContent', 'DummyModel', 'Injection'];
+
+        $observerInputs = [$observerNamespace, $observerName, $observerContent, $observerModel, $observerModelInjection];
+
+        $observerText = str_replace($dummyStrings, $observerInputs, $observerStub);
+
+        if (Storage::disk('observers')->put($observerDirectory . '.php', $observerText)) {
+
+            $this->info('Observer was successfully created');
+
+        } else {
+
+            $this->error('Observer was not created!');
+
+        }
+    }
+}
---
+++ src/MakeObserverCommandServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace NickSynev\MakeObserverCommand;
+
+use Illuminate\Support\ServiceProvider;
+
+class MakeObserverCommandServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+
+    protected $commands = [
+        'NickSynev\MakeObserverCommand\MakeObserver',
+    ];
+
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands($this->commands);
+    }
+}
